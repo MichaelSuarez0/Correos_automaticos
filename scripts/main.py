57d8d77bc949f2d362f5535b88c0abcca79e714d
Michael Suárez <a20180264@pucp.edu.pe>
2025-02-21 17:56:51 -0500

    Update main.py
     - Simplified internal dicts for improved readability.
    - Cleaned up redundant code

--- scripts/main.py
+++ scripts/main.py
@@ -2,25 +2,15 @@
 import os
 import re
 import json
-import sys
-from pathlib import Path, PurePath
 from shutil import move
 from datetime import date, timedelta
 from dotenv import load_dotenv
-from exchangelib import Account, Credentials, DELEGATE
-from office365.runtime.auth.authentication_context import AuthenticationContext
-from office365.runtime.auth.user_credential import UserCredential
-from office365.sharepoint.client_context import ClientContext
-from office365.sharepoint.files.file import File
-import imaplib
-import email
-from email.header import decode_header
 from correos_automaticos.classes.outlook_manager import OutlookRetriever, OutlookSender
-from correos_automaticos.classes.file_manager import FileManager
+from correos_automaticos.classes.file_manager import SUBJECT_FILTER, FileManager
 from correos_automaticos.classes.sharepoint_manager import Sharepoint
-
-import pprint
+from pprint import pprint
 from collections import defaultdict
+from icecream import ic
 
 # 1: Root directory to upload files
 #ROOT_DIR = sys.argv[1]
@@ -35,21 +25,17 @@ load_dotenv()
 script_dir = os.path.dirname(__file__)
 
 # Configuración global
-IMAP_SERVER = os.getenv("IMAP_SERVER")  
-OUTLOOK_EMAIL = os.getenv("OUTLOOK_EMAIL")
-OUTLOOK_PASSWORD = os.getenv("OUTLOOK_PASSWORD")
-SUBJECT_FILTER = os.getenv("SUBJECT_FILTER")
+SHAREPOINT_URL = "https://ceplangobpe.sharepoint.com/sites/DNPE"
+SHAREPOINT_FOLDER = "Documentos compartidos/AOI Tendencias/Prueba"  # Ruta del folder de destino
 DOWNLOAD_PATH = os.path.join(script_dir, "..", 'descargas')
-IMAP_PORT = os.getenv("IMAP_PORT")  # Puerto para conexión segura
-
+SUBJECT_FILTER = "Sistematizar"
 
 # Diccionario para clasificar códigos de fichas
 ruta_json_1 = os.path.join(script_dir, "..", "..", 'datasets', "rubros_subrubros.json")
 with open(ruta_json_1, "r", encoding='utf-8') as file:
     rubros_subrubros = json.load(file)
 
-
-# Diccionario para buscar metadata de las fichas y renombrar archivos a partir de "Título largo"
+# Diccionario para renombrar archivos a partir de "Título largo"
 ruta_json_2 = os.path.join(script_dir, "..", "..", 'datasets', "info_obs.json")
 with open(ruta_json_2, "r", encoding='utf-8') as file:
     info_obs = json.load(file)
@@ -60,7 +46,7 @@ ruta_renombrados= os.path.join(DOWNLOAD_PATH, "clasificados")
 # 1. Definir funciones subordinadas
 def find_file_path(file_name, regex_dict):
     """
-    Clasifica un archivo según su nombre a partir de un diccionario de categorías.
+    Clasifica una ficha según su nombre a partir de un diccionario de categorías.
 
     Args:
         file_name (str): El nombre del archivo a clasificar.
@@ -88,7 +74,7 @@ def find_file_path(file_name, regex_dict):
     return ""
 
 
-def construct_log_dict(email_data, renamed_files_map):
+def construct_log_dict(email_data: dict, renamed_files_map: dict):
     """
     Reconstructs email_data dict to another dict suitable for sending confirmation emails to users based on uploaded attachments
 
@@ -106,12 +92,12 @@ def construct_log_dict(email_data, renamed_files_map):
         sender = details.get("from_email")
 
         if not sender:
-            print("No sender found for email details:", details)  # Depuración
+            print("No sender found for email details:", details) 
             continue
 
         # Inicializar diccionario para el remitente si no existe
         if sender not in user_attachments_log:
-            user_attachments_log[sender] = {}
+            user_attachments_log[sender] = []
         
         # Obtener lista de adjuntos
         attachments = details.get("attachments", [])
@@ -131,29 +117,17 @@ def construct_log_dict(email_data, renamed_files_map):
             new_file_path = find_file_path(new_file_name, rubros_subrubros)
             
             # Construir la entrada del archivo
-            user_attachments_log[sender][old_file_name] = {
+            user_attachments_log[sender].append({
                 "original_name": old_file_name,
                 "new_name": new_file_name,
                 "path": new_file_path
-            }
+            })
 
     return user_attachments_log
 
-# Imprimir de manera legible
-def print_dict(d):
-    """
-    Imprime un diccionario de manera más legible.
-    """
-    for sender, attachments in d.items():
-        print(f"\nRemitente: {sender}")
-        if attachments:
-            for file_name, path in attachments.items():
-                print(f"  Archivo: {file_name} -> Ruta: {path}")
-        else:
-            print("  No hay archivos clasificados.")
 
 #print(find_file_category("tg1 ejemplo.txt", rubros_subrubros))  # Debería imprimir: "Tendencias/Tendencias Globales"
-print(find_file_path("r23_madre ejemplo.txt", rubros_subrubros))  # Debería imprimir: "Tendencias/Tendencias Territoriales/Madre de Dios"
+#print(find_file_path("r23_madre ejemplo.txt", rubros_subrubros))  # Debería imprimir: "Tendencias/Tendencias Territoriales/Madre de Dios"
 
 
 # ## 2. Definir funciones principales
@@ -168,7 +142,7 @@ def obtener_archivos(start_date: str):
     """
     outlook_session = OutlookRetriever()
     outlook_session._auth()
-    email_data = outlook_session.get_emails(start_date=start_date)
+    email_data = outlook_session.get_emails(start_date=start_date, subject_filter=SUBJECT_FILTER)
     outlook_session.download_attachments(email_data)
     return email_data
 
@@ -188,21 +162,22 @@ def renombrar_y_clasificar(search_directory = DOWNLOAD_PATH, email_data = {}):
     renamed_files_map = file_manager.rename_files(info_obs)
     #lista_nombres_archivos = FileManager(ruta_renombrados).list_files()
     user_attachments_log = construct_log_dict(email_data=email_data, renamed_files_map=renamed_files_map)
-    return renamed_files_map, user_attachments_log
+    return user_attachments_log
 
 
 
-def upload_files_to_sharepoint(user_attachments_log):
-    sharepoint_session = Sharepoint()
+def upload_files_to_sharepoint(user_attachments_log: dict):
+    sharepoint_session = Sharepoint(SHAREPOINT_URL, SHAREPOINT_FOLDER)
     sharepoint_session._auth()
 
-    for sender, file_dict in user_attachments_log.items():
-        for old_file_name, attachment_details in file_dict.items():
+    for sender, file_list in user_attachments_log.items():
+        for attachment_details in file_list:
             path = attachment_details.get("path")
+            custom_folder_path = f'{SHAREPOINT_FOLDER}/{path} '
             new_file_name  = attachment_details.get("new_name")
             try:
                 # Subir archivo a Sharepoint
-                sharepoint_session.upload_file(new_file_name, path)
+                sharepoint_session.upload_file(new_file_name, custom_folder_path, create_folder=True)
                 # Actualizar estado en el log
                 attachment_details["sharepoint_status"] = "uploaded"
             except Exception as e:
@@ -217,11 +192,14 @@ def send_confirmation_emails(user_attachments_log):
 
 
 def main(start_date: str):
-    email_data = obtener_archivos(start_date=start_date) # OutlookRetriever
-    renamed_files_map, user_attachments_log = renombrar_y_clasificar(email_data=email_data) # FileManager
-    upload_files_to_sharepoint(user_attachments_log) # Sharepoint
-    send_confirmation_emails(user_attachments_log) # OutlookSender
-    return email_data, user_attachments_log, renamed_files_map
+
+    email_data = obtener_archivos(start_date)                                  # OutlookRetriever
+    user_attachments_log = renombrar_y_clasificar(DOWNLOAD_PATH, email_data)   # FileManager
+    upload_files_to_sharepoint(user_attachments_log)                         # Sharepoint
+    send_confirmation_emails(user_attachments_log)                           # OutlookSender
+    # return email_data, user_attachments_log, renamed_files_map
+    # ic(email_data)
+    # ic(user_attachments_log)
 
 if __name__ == "__main__":
-    main("1-Jan-2025")
+    main("21-Feb-2025")
